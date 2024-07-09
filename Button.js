f3520e76b47e3f77d80f7a8148cfe83a7ac41dd1
Sam W <samuel.l.winterton@gmail.com>
2024-07-09 19:51:52 +0100

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,7 +1,7 @@
 function checkPswd() {
-        var confirmWord = "Fart";
-        var word = document.getElementById("pswd").value;
-        if (word == confirmPassword) {
+        var confirmPassword = "Fart";
+        var password = document.getElementById("pswd").value;
+        if (password == confirmPassword) {
              window.location.assign("Home.html")
         }
         else{
